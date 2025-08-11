eb170254efd6e4e855b28c6087648e5c403e3e77
Franck-Sorel <mfrancksorel@gmail.com>
2025-08-11 15:50:52 +0100

    Delete tf-aigateway directory

--- tf-aigateway/config.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "envoy_gateway_config" {
-  provisioner "local-exec" {
-    command = "kubectl apply --force -f https://raw.githubusercontent.com/envoyproxy/ai-gateway/main/manifests/envoy-gateway-config/config.yaml || true"
-  }
-  depends_on = [null_resource.redis_config]
-}
--- tf-aigateway/restart.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "restart_envoy_gateway" {
-  provisioner "local-exec" {
-    command = <<EOT
-kubectl rollout restart -n ${var.envoy_gateway_namespace} deployment/envoy-gateway && \
-kubectl wait --timeout=2m -n ${var.envoy_gateway_namespace} deployment/envoy-gateway --for=condition=Available
-EOT
-  }
-  depends_on = [null_resource.envoy_gateway_rbac]
-}
