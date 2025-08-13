5a7ecaebb2df48083271b7b85a7fb090e0fa7deb
benie-joy-possi <beniejoypossi@gmail.com>
2025-08-13 14:27:07 +0100

    feat(s3): output multi-user credentials

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,26 @@ output "argocd_server_url" {
   value     = "https://${local.argocdDomain}"
   sensitive = false
 }
+
+output "s3_buckets" {
+  description = "Information about created S3 buckets and their users"
+  value = {
+    for bucket_name, bucket_module in module.s3_buckets : bucket_name => {
+      bucket_name = bucket_module.bucket_name
+      bucket_arn  = bucket_module.bucket_arn
+      users       = bucket_module.users
+    }
+  }
+}
+
+# Sensitive output for all credentials organized by bucket and user
+output "s3_bucket_credentials" {
+  description = "Sensitive credentials for all S3 bucket users"
+  value = {
+    for bucket_name, bucket_module in module.s3_buckets : bucket_name => {
+      bucket_name = bucket_module.bucket_name
+      users       = bucket_module.user_credentials
+    }
+  }
+  sensitive = true
+}
