a07ae3b03b625bf5a652d43e9d7cb6399a8a8185
dericko681 <abonghoderick@gmail.com>
2025-08-14 11:28:32 +0100

    chore: update version of helm and kubernetes to newer

--- tf-kserve/providers.tf
+++ tf-kserve/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0"
+      version = ">= 2.9.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = "~> 2.20"
     }
   }
 }
