296be6f69bdfc341e4f0f9e2f8d102d7e230b84e
franck-sorel <mfrancksorel@gmail.com>
2025-08-18 15:37:56 +0100

    fix: --rootPrefix argument error has been fixed

--- tf-aigateway/main.tf
+++ tf-aigateway/main.tf
@@ -34,132 +34,111 @@ provider "helm" {
   }
 }
 
-# module "ai_gateway_crds" {
-#   source  = "terraform-module/release/helm"
-#   version = ">= 2.9.1"
+ module "ai_gateway_crds" {
+    source  = "terraform-module/release/helm"
+    version = ">= 2.9.1"
 
-#   repository = "oci://docker.io/envoyproxy"
-#   namespace  = var.ai_gateway_namespace
+    repository = "oci://docker.io/envoyproxy"
+    namespace  = var.ai_gateway_namespace
 
-#   app = {
-#     name    = "aieg-crd"
-#     version = var.chart_version
-#     chart   = "ai-gateway-crds-helm"
-#     create_namespace = true
-#     deploy = 1
+    app = {
+      name    = "aieg-crd"
+      version = var.chart_version
+      chart   = "ai-gateway-crds-helm"
+      create_namespace = true
+      deploy = 1
 
-#   }
+    }
 
-#   set = [
-#     {
-#       name  = "aiGateway.enabled"
-#       value = "false"
-#     }
-#   ]
+    set = []
 
-#   values = []
+    values = []
 
-#   depends_on = []
-# }
+    depends_on = []
+  }
 
 module "envoy_gateway" {
-  source  = "terraform-module/release/helm"
-  version = ">= 2.9.1"
+   source  = "terraform-module/release/helm"
+   version = ">= 2.9.1"
 
-  repository = "oci://docker.io/envoyproxy"
-  namespace  = var.envoy_gateway_namespace
+   repository = "oci://docker.io/envoyproxy"
+   namespace  = var.envoy_gateway_namespace
 
-  app = {
-    name    = "eg"
-    version = var.chart_version
-    chart   = "gateway-helm"
-    deploy = 1
-    create_namespace = true
-  }
+   app = {
+     name    = "eg"
+     version = var.chart_version
+     chart   = "gateway-helm"
+     deploy = 1
+     create_namespace = true
+   }
 
-  set = [
-    {
-      name  = "aiGateway.enabled"
-      value = "false"
-    }
-  ]
+   set = []
 
-  values = [file("./values/config.yaml")]
+   values = [file("./values/config.yaml")]
 
-  depends_on = [module.ai_gateway]
-}
+   depends_on = [module.ai_gateway_crds]
+ }
 
 module "ai_gateway" {
-  source  = "terraform-module/release/helm"
-  version = ">= 2.9.1"
+   source  = "terraform-module/release/helm"
+   version = ">= 2.9.1"
 
-  repository = "oci://docker.io/envoyproxy"
-  namespace  = var.ai_gateway_namespace
+   repository = "oci://docker.io/envoyproxy"
+   namespace  = var.ai_gateway_namespace
 
-  app = {
-    name    = "aieg"
-    version = var.chart_version
-    chart   = "ai-gateway-helm"
-    deploy = 1
-    create_namespace = true
-    wait    = false
-  }
+   app = {
+     name    = "aieg"
+     version = var.chart_version
+     chart   = "ai-gateway-helm"
+     deploy = 1
+     create_namespace = true
+     wait    = false
+   }
 
-  set = [
-    {
-      name  = "aiGateway.enabled"
-      value = "false"
-    },
-  {
-    name  = "endpointConfig.rootPrefix"
-    value = ""
-  }
+   set = []
 
+   values = []
 
-  ]
-
-  values = [/* file("./values/aieg.yaml") */]
-
-  depends_on = [/* module.ai_gateway_crds */]
-}
+   depends_on = [module.ai_gateway_crds, module.envoy_gateway]
+ }
 
 module "redis" {
-  source = "terraform-module/release/helm"
-  version = ">= 2.9.1"
+   source = "terraform-module/release/helm"
+   version = ">= 2.9.1"
 
-  repository = "https://charts.bitnami.com/bitnami"
-  namespace  = var.redis_namespace
+   repository = "https://charts.bitnami.com/bitnami"
+   namespace  = var.redis_namespace
 
-  app = {
-    name       = "redis"
-    chart      = "redis"
-    version    = "19.0.1"
-    deploy     = 1
-    create_namespace = true
-  }
+   app = {
+     name       = "redis"
+     chart      = "redis"
+     version    = "19.0.1"
+     deploy     = 1
+     create_namespace = true
+   }
 
-  values = []
+   values = []
 
-  depends_on = [module.envoy_gateway]
-}
+   depends_on = [module.envoy_gateway, module.ai_gateway, module.ai_gateway_crds]
+ }
 
 
 module "envoy_gateway_config" {
-  source  = "terraform-module/release/helm"
-  version = ">= 2.9.1"
+   source  = "terraform-module/release/helm"
+   version = ">= 2.9.1"
 
-  repository = ""
-  namespace  = var.envoy_gateway_namespace
+   repository = ""
+   namespace  = var.envoy_gateway_namespace
 
-  app = {
-    name    = "envoy-gateway-config"
-    version = "0.1.0"
-    chart   = "./charts/envoy-gateway-config"
-    deploy  = 1
-    # force_update     = true
-  }
+   app = {
+     name    = "envoy-gateway-config"
+     version = "0.1.0"
+     chart   = "./charts/envoy-gateway-config"
+     deploy  = 1
+     # force_update     = true
+   }
 
-  values = []
+   values = []
 
-  depends_on = [module.envoy_gateway, module.ai_gateway]
-}
+   depends_on = [module.ai_gateway_crds, module.envoy_gateway, module.ai_gateway, module.ai_gateway_crds]
+ }
