3c31c0f52ff18ebeae2e1bbe023bab79127cd8e4
franck-sorel <mfrancksorel@gmail.com>
2025-08-20 14:41:34 +0100

    ADD: Adding providers.tf

---
+++ tf-aigateway/providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.31.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                    = var.kube_host
+  cluster_ca_certificate  = base64decode(var.kube_cluster_ca_certificate)
+  client_certificate      = base64decode(var.kube_client_certificate)
+  client_key              = base64decode(var.kube_client_key)
+  token                   = var.kube_token
+}
+
+provider "helm" {
+
+  kubernetes = {
+  host                   = var.kube_host
+  cluster_ca_certificate = base64decode(var.kube_cluster_ca_certificate)
+  client_certificate     = base64decode(var.kube_client_certificate)
+  client_key             = base64decode(var.kube_client_key)
+  token                  = var.kube_token
+  }
+}
