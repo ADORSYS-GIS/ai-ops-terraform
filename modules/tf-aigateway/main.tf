9d11716925a63bd34978330c4d585cfedba47219
franck-sorel <mfrancksorel@gmail.com>
2025-08-21 16:24:13 +0100

    update: update providers versions

--- modules/tf-aigateway/main.tf
+++ modules/tf-aigateway/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 3.0"
+      version = ">= 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.31.0"
+      version = ">= 2.0"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = ">= 2.0.0"
+      version = ">= 2.0"
     }
   }
 }
