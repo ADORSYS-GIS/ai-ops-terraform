87e98fc0a9ac7e5d014129d063c85c29af19a147
franck-sorel <mfrancksorel@gmail.com>
2025-08-21 16:01:16 +0100

    update: update providers versions

--- modules/argocd/providers.tf
+++ modules/argocd/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 3.0"
+      version = ">= 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.31.0"
+      version = ">= 2.0"
     }
   }
 }
