907edd53000249887e6091bd2b526d3207f55583
dericko681 <abonghoderick@gmail.com>
2025-08-21 11:02:16 +0100

    chore: update file to remove unneccessary refences

--- ami_builder.tf
+++ ami_builder.tf
@@ -4,7 +4,7 @@ module "custom_karpenter_ami" {
   ami_name                  = "karpenter-custom"
   base_ami_version          = "1.28" # Corresponds to an EKS version
   build_instance_type       = "t3.large"
-  customization_script_path = "${path.module}/files/provisioner.sh"
+  
   component_version         = "1.0.1"
 
   subnet_id          = module.vpc.private_subnets[0]
@@ -14,18 +14,4 @@ module "custom_karpenter_ami" {
     Project   = "AI-TF"
     ManagedBy = "Terraform"
   }
-}
-
-# The AMI is built asynchronously. Use a data source to get the ID of the
-# latest successful AMI built by the pipeline.
-data "aws_imagebuilder_image" "latest_custom_ami" {
-  arn = module.custom_karpenter_ami.image_pipeline_arn
-
-  # This ensures the data source is refreshed when the pipeline is updated.
-  depends_on = [module.custom_karpenter_ami]
-}
-
-output "latest_custom_ami_id" {
-  description = "The ID of the latest custom AMI built by the pipeline."
-  value       = data.aws_imagebuilder_image.latest_custom_ami.image_id
 }
