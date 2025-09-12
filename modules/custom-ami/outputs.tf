a4ce0405814247addb9ce9cb17240f79457119b8
tegha-romeo <romeotegha@gmail.com>
2025-09-12 11:24:04 +0100

    feat(custom-ami): expose ami_id and ami_name outputs

--- modules/custom-ami/outputs.tf
+++ modules/custom-ami/outputs.tf
@@ -2,3 +2,13 @@ output "image_pipeline_arn" {
   description = "The ARN of the EC2 Image Builder pipeline."
   value       = aws_imagebuilder_image_pipeline.this.arn
 }
+
+output "ami_id" {
+  description = "The ID of the built AMI."
+  value       = try(aws_imagebuilder_image.this.output_resources[0].amis[0].image, null)
+}
+
+output "ami_name" {
+  description = "The name of the built AMI."
+  value       = try(aws_imagebuilder_image.this.output_resources[0].amis[0].name, null)
+}
