20757bd2688372e78ec13714c50beafc682b1271
tegha-romeo <romeotegha@gmail.com>
2025-09-12 11:25:05 +0100

    chore(ami): align base_ami_version with var.cluster_version

--- ami_builder.tf
+++ ami_builder.tf
@@ -1,14 +1,14 @@
 module "custom_karpenter_ami" {
   source = "./modules/custom-ami"
 
-  ami_name                  = "karpenter-custom"
-  base_ami_version          = "1.28" # Corresponds to an EKS version
-  build_instance_type       = "t3.large"
-  
-  component_version         = "1.0.1"
+  ami_name            = "karpenter-custom"
+  base_ami_version    = var.cluster_version # Align to EKS version
+  build_instance_type = "t3.large"
+
+  component_version = "1.0.1"
 
   subnet_id          = module.vpc.private_subnets[0]
-  security_group_ids = [aws_security_group.default.id]
+  security_group_ids = []
 
   tags = {
     Project   = "AI-TF"
