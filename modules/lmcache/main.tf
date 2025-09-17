3edb2e33ca36305c510dca516270b53bca4ce01c
dericko681 <abonghoderick@gmail.com>
2025-09-17 09:01:31 +0100

    feat: remove the lmcache helm chart to the dedicated repo

--- modules/lmcache/main.tf
+++ modules/lmcache/main.tf
@@ -2,8 +2,9 @@ resource "helm_release" "lmcache_kserve_inference" {
   name       = var.release_name
   namespace  = var.namespace
   
-  # Specify the path to your local Helm chart
-  chart      = "${path.module}/lmcache-kserve-inference"
+  # Use chart from GitHub repository
+  repository = "https://github.com/ADORSYS-GIS/ai-helm"
+  chart      = "charts/lmcache-kserve-inference"
   
   values = [
     templatefile("${path.module}/values.yaml", {
@@ -12,4 +13,3 @@ resource "helm_release" "lmcache_kserve_inference" {
     })
   ]
 }
-
