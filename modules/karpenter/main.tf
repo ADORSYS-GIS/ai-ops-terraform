a78760e117bd09bf22e69c3af1242df9e73caa7e
Franck-Sorel <mfrancksorel@gmail.com>
2025-09-29 13:09:48 +0100

    fix(): AWS provider version mismatch by downgrading Karpenter module version to 20.0.0 [skip ci]

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -1,6 +1,6 @@
 module "karpenter" {
   source                = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version               = "~> 21.3"
+  version               = "~> 20.0"
   cluster_name          = var.cluster_name
   enable_v1_permissions = true
   create_pod_identity_association = true
