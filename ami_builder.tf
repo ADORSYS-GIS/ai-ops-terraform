08ef6f28deff99aabe2b799f1fd945654f88406a
benie-joy-possi <beniejoypossi@gmail.com>
2025-10-16 09:39:09 +0100

    feat(ami): Add K-Server custom AMI and refine Karpenter AMI module

--- ami_builder.tf
+++ ami_builder.tf
@@ -6,6 +6,25 @@ module "custom_karpenter_ami" {
   build_instance_type       = "t3.large"
   
   component_version         = "1.0.1"
+  customization_script_path = null
+
+  subnet_id          = module.vpc.private_subnets[0]
+  security_group_ids = [aws_security_group.default.id]
+
+  tags = {
+    Project   = "AI-TF"
+    ManagedBy = "Terraform"
+  }
+}
+
+module "k_server_custom_ami" {
+  source = "./modules/custom-ami"
+
+  ami_name                  = "k-server-custom"
+  base_ami_version          = "1.28" # Corresponds to an EKS version
+  build_instance_type       = "t3.large"
+  component_version         = "1.0.0"
+  customization_script_path = "${path.module}/files/k_server_customization_script.sh"
 
   subnet_id          = module.vpc.private_subnets[0]
   security_group_ids = [aws_security_group.default.id]
@@ -29,3 +48,4 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-egress-sgr
   }
 }
+
